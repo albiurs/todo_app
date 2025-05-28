cf752bdcefef55b8879e3bc9ea236791edad6a08
Urs Albisser <u.albisser@gmail.com>
2025-05-28 08:32:03 +0200

    update

--- main1_print-input-str-type-vars-array.py
+++ main1_print-input-str-type-vars-array.py
@@ -1,11 +1,12 @@
-user_prompt = "Enter todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
+user_prompt = "Enter todo"
+todo1 = input(user_prompt + "1: ")
+todo2 = input(user_prompt + "2: ")
+todo3 = input(user_prompt + "3: ")
 
 todos = [todo1, todo2, todo3]
 print(todos)
 
+# Debug
 print("Type of the user_prompt: " + str(type(user_prompt)))
 print("Type of the todo1: " + str(type(todo1)))
 print("Type of the todos: " + str(type(todos)))
